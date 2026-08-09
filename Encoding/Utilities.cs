df3f2fd84e1046f3ed7e8af487af2e8de6fc50d7
yelli77 <munterreitmeier@gmail.com>
2026-08-09 10:06:56 +0200

    Utilities.cs (original, base for our patches - see CHANGES.md)

---
+++ Encoding/Utilities.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace StarTruckMP.Utilities
+{
+    public enum messageType
+    {
+        clientJoin,
+        clientDisconnect,
+        movementUpdate,
+        chatMessage,
+        updateSector,
+        updateLivery
+    }
+
+    public struct playerInfo
+    {
+        public GameObject Player;
+        public GameObject Truck;
+        public movementTrans playerTrans;
+        public movementTrans truckTrans;
+        public string Name;
+        public string sector;
+        public bool seated;
+        public string livery;
+    }
+
+    public struct movementTrans
+    {
+        public Vector3 Pos;
+        public Vector3 Rot;
+        public Vector3 Vel;
+        public Vector3 AngVel;
+    }
+
+    internal class Utilities
+    {
+    }
+}
