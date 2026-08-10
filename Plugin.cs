48626fac103bb989339e822f26fc1316186c848d
root <root@srv1628969.hstgr.cloud>
2026-08-10 16:26:25 +0000

    custom-build-60: map player indicators - diagnostic build

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,7 @@ public class StarTruckMP : BasePlugin
     public const string pluginGuid = "StarTruckMP";
     public const string pluginName = "Star Trucker MP";
     public const string pluginVersion = "0.1";
-    public const string customBuildNumber = "custom-build-56";
+    public const string customBuildNumber = "custom-build-60";
     internal static new ManualLogSource Log;
     public static ConfigEntry<string> IPAddress;
     public static ConfigEntry<int> MoveUpdate;
