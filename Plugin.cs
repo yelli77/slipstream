4f1b160344b958a4811f2b4def0c177e5a1c6b35
root <root@srv1628969.hstgr.cloud>
2026-08-15 16:40:36 +0000

    custom-build-161: sync display version + release artifacts

--- Plugin.cs
+++ Plugin.cs
@@ -21,7 +21,7 @@ public class StarTruckMP : BasePlugin
     // WICHTIG: bei jedem Release-Build hochzaehlen (siehe version.json) - customBuildNumber ist
     // nur ein Anzeige-String, protocolBuildNumber ist die tatsaechlich fuer den Versionscheck
     // gegen den Server verwendete Zahl.
-    public const string customBuildNumber = "custom-build-160";
+    public const string customBuildNumber = "custom-build-161";
     public const int protocolBuildNumber = 151;
     internal static new ManualLogSource Log;
 
