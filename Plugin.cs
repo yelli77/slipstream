84bac0bf3c5888b6fcc7d234bf4dccae3c12c84b
root <root@srv1628969.hstgr.cloud>
2026-09-13 07:38:49 +0000

    custom-build-312 bump

--- Plugin.cs
+++ Plugin.cs
@@ -21,7 +21,7 @@ public class StarTruckMP : BasePlugin
     // WICHTIG: bei jedem Release-Build hochzaehlen (siehe version.json) - customBuildNumber ist
     // nur ein Anzeige-String, protocolBuildNumber ist die tatsaechlich fuer den Versionscheck
     // gegen den Server verwendete Zahl.
-    public const string customBuildNumber = "custom-build-311";
+    public const string customBuildNumber = "custom-build-312";
     public const int protocolBuildNumber = 152;
     internal static new ManualLogSource Log;
 
