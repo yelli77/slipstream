0f1fd04c8d8ad28646e7958a5805686f6f940ef1
architect <architect@hermes.local>
2026-09-17 18:12:47 +0000

    debug(test): srv-upload-Parsing temporär

--- tests/JobBoardSmokeTest/Program.cs
+++ tests/JobBoardSmokeTest/Program.cs
@@ -152,7 +152,7 @@ public static class Program
         _server = new Riptide.Server();
         _server.ClientConnected += (s, e) => { Console.WriteLine($"[server] Client connected: {e.Client.Id}"); _players[e.Client.Id] = new PlayerState { Id = e.Client.Id, Sector = "none" }; };
         _server.ClientDisconnected += (s, e) => _players.Remove(e.Client.Id);
-        _server.MessageReceived += (s, e) => { Console.WriteLine($"[srv-msg] id={e.MessageId} from={e.FromConnection.Id} bytes={e.Message.GetBytes(0).Length}"); _handler.Handle(e, _server); };
+        _server.MessageReceived += (s, e) => { if (e.MessageId == 19) { try { var sec = e.Message.GetString(); var sid = e.Message.GetUShort(); var tid = e.Message.GetByte(); var ci = e.Message.GetUShort(); var tc = e.Message.GetUShort(); var data = e.Message.GetBytes(); Console.WriteLine($"[srv-upload] sector={sec} sender={sid} tid={tid} ci={ci}/{tc} len={data.Length}"); } catch (Exception ex) { Console.WriteLine($"[srv-upload] PARSE-FEHLER: {ex.Message}"); } } _handler.Handle(e, _server); };
         _server.Start(ServerPort, 8);
         _store = new JobBoardStore(LogServer);
         _handler = new MessageHandler(_players, 1, _ => { }, (_, _) => { });
